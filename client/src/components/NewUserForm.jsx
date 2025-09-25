5f1e2e00c1bb8e5d6d0d28a72c455a4810c8665b
Owain Culley (culleyo) <culleyo@coventry.ac.uk>
2025-09-25 18:23:24 +0100

    Added frontend form requirements and README

--- client/src/components/NewUserForm.jsx
+++ client/src/components/NewUserForm.jsx
@@ -12,8 +12,21 @@ function UserForm({ onSubmit, onCancel }) {
   };
 
   const handleSubmit = (e) => {
-    console.log(formData)
     e.preventDefault();
+    const dob = new Date(formData.dob);
+    const current = new Date();
+    const hundredYearsAgo = new Date();
+    hundredYearsAgo.setFullYear(current.getFullYear() - 100);
+    if (dob > current) {
+      alert("Date of birth cannot be in the future.");
+      return;
+    }
+
+    if (dob < hundredYearsAgo) {
+      alert("Date of birth cannot be more than 100 years ago.");
+      return;
+    }
+
     onSubmit(formData);
   };
 
@@ -22,8 +35,8 @@ function UserForm({ onSubmit, onCancel }) {
     <div className="form-overlay">
         <div className="form-content">
             <form onSubmit={handleSubmit}>
-            <input name="first_name" placeholder="First Name" onChange={handleChange} required />
-            <input name="last_name" placeholder="Last Name" onChange={handleChange} required />
+            <input name="first_name"  pattern="[A-Za-z\s\-']+" title="Name can only contain letters" maxLength={35} placeholder="First Name" onChange={handleChange} required />
+            <input name="last_name" pattern="[A-Za-z\s\-']+" title="Name can only contain letters" maxLength={35} placeholder="Last Name" onChange={handleChange} required />
             <input name="dob" type="date" onChange={handleChange} required />
             <button type="submit">Create</button>
             <button type="button" onClick={onCancel}>Cancel</button>
